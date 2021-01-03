abe11453cd55ef4bbfe24908825d14f816d4cfdb
AndrewVolosevich <volosevich1989@gmail.com>
2021-01-03 21:32:15 +0300

    feat: added auth to the project

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,38 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { BadRequestException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+
+  private static async hashPassword(password: string, salt: string): Promise<string> {
+    return await bcrypt.hash(password, salt)
+  }
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const {username, password} = authCredentialsDto
+
+    const isExist = await this.findOne({username})
+    if(isExist) {
+      throw new BadRequestException({message: 'Wrong credentials'})
+    }
+
+    const user = new User()
+    user.username = username
+    user.salt = await bcrypt.genSalt()
+    user.password = await this.hashPassword(password, user.salt)
+    await user.save()
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const {username, password} = authCredentialsDto
+    const user = await this.findOne({username})
+    if (user && await user.validatePassword(password)) {
+      return user.username
+    } else if (!user || !await user.validatePassword(password)) {
+      throw new BadRequestException({message: 'Wrong credentials'})
+    }
+  }
+}
